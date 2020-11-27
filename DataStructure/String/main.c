4e9b01f765b0599ca720cc338aae8e8c5f25c840
penguin1996 <penguins1996@163.com>
2020-11-27 13:44:47 +0800

    add the C++ project

--- DataStructure/String/main.c
+++ DataStructure/String/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 #define MAXLEN 255 //预定最大的串长度255。
 
 //静态数组定义串，静态数组实现定长顺序存储，当有关静态数组实现的代码执行成功后，其所占的空间会自动被回收。
-struct{
+typedef  struct{
     char ch[MAXLEN]; //每个分量存储一个字符串。
     int length;  //串的实际长度。
 }SString;
 
 //动态数组定义串,动态数组实现是基于堆分配存储，当有关动态数组的代码执行完之后，堆空间需要通过free函数手动回收。
-struct{
+typedef struct{
     char *ch;
     int length;
 }HString;
@@ -19,6 +21,44 @@ struct{
  * S.ch = (char *) malloc(MAXLEN * sizeof(char));
  * S.length = 0;
  * */
+
+//求子串,用Sub返回串S的第pos个字符起长度为len的子串
+bool SubString(SString *Sub,SString S, int pos, int len){
+
+    //子串范围越界
+    if(pos+len-1 > S.length){
+        return false;
+    }
+
+    for(int i = pos; i < pos + len; i++){
+        Sub->ch[i-pos+1] = S.ch[i];
+        Sub->length = len;
+    }
+    return true;
+}
+
+//子串与主串的比较操作。若S>T,则返回值 > 0; 若S = T, 则返回值 = 0； 若S < T, 则返回值 < 0
+int StrCompare(SString S,SString T) {
+    for (int i = 1; i <= S.length && i <= T.length; i++) {
+        if(S.ch[i] != T.ch[i]) {
+            return S.ch[i] - T.ch[i];
+        }
+    }
+    //扫描过的所有字符都相同，则长度长的串更大
+    return S.length - T.length;
+}
+
+//定位操作。若主串S中存在与串T值相同的子串，则返回它在主串S中第一次出现的位置，否则函数值为0.
+int Index(SString S, SString T) {
+    int i = 1, n = S.length, m = T.length;
+    SString *sub;
+    while(i <= n-m+1) {
+        SubString(sub,S,i,m);
+        if(StrCompare(*sub,T) != 0) ++i;
+        else return i; //返回子串在主串的位置
+    }
+    return 0;   //S中不存在与T相同的子串
+}
 
 int f(char *s) {
     int n = 0, i;
---
+++ DataStructure/patternMatch/cmake-build-debug/CMakeFiles/patternMatch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/patternMatch.dir/main.cpp.obj"
+  "patternMatch.pdb"
+  "patternMatch.exe"
+  "patternMatch.exe.manifest"
+  "libpatternMatch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/patternMatch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
